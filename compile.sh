94d45ab1ea00f1ee869df7a991a1304843f9610c
Blaine Mooers <bmooers1@gmail.com>
2024-10-25 07:29:06 -0500

    Updated.

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@
 # Has to be run in the main directory with the main.aux generated after the full running of the compile.sh script.
 # bibtool -x main.aux -o ./AnnotatedBibliography/AnnoBibMyBDA.bib
 
-echo "Run from the abibXXXX subfolder"
+echo "Run from the mabibXXXX subfolder"
 if [ $# -lt 1 ]; then
   echo 1>&2 "$0: not enough arguments"
   echo "Usage1: ./compile.sh projectIndexNumber"
@@ -21,15 +21,15 @@ fi
 
 cd ../
 bibtool -x main.aux -o ./abib$1/ab$1.bib
-cd ./abib$1
-pdflatex ab$1
-bibtex ab$1
-pdflatex ab$1
-pdflatex ab$1
+cd ./mabib$1
+pdflatex mabib$1
+biber mabib$1
+pdflatex mabib$1
+pdflatex mabib$1
 open -a "Preview.app" v.pdf
 echo "================================================"
 echo "All done! ab'$1'.pdf has been created. - Blaine"
 echo "================================================"
 
 # clean:
-rm -rf ab$1.log ab$1.blg ab$1.bbl ab$1.aux
+rm -rf mabib$1.log mabib$1.blg mabib$1.bbl mabib$1.aux
